82a5525577232d1d2733ea926412e24de92ef555
Cody Brooks <codymb36@gmail.com>
2013-10-19 22:10:32 -0500

    Added prints for testing purposes

--- sumpPumpMKI_testing.py
+++ sumpPumpMKI_testing.py
@@ -1,3 +1,5 @@
+print ("The Script has started")
+
 import time
 from datetime import datetime
 now = datetime.time
@@ -27,13 +29,13 @@ def sendEmail(msg):
     message as well as the log file attached."""
     #enter the code that sends an email to the family with the log attached
 
-    fromaddr = 'from@email.com'
-    toaddrs = ['to@email.com']
+    fromaddr = 'brookssumppump@gmail.com'
+    toaddrs = ['codymb36@gmail.com']
     finalmsg = msg + """Please see the Pi and the data log file for more details."""
 
     # Credentials (if needed)
-    username = 'my_username'
-    password = 'my_password'
+    username = 'brookssumppump'
+    password = '!sumppump*'
 
     finalmsg.attached()
 
@@ -49,19 +51,25 @@ while running is True:
     if floatSwitch is True:
         #Write the time and what happened to the file
         log.write(str(now) + " Float switch turned on")
+        print ("Float switch turned on")
         #Wait until switch is turned off
 
         while floatSwitch:
             startTime = time.time()
             if floatSwitch is False:
                 log.write(str(now) + " Float switch turned off")
+                print ("Float switch turned off")
                 break
             #if elapsedTime > 3 min (in the form of 180 seconds)
             elif elapsedTime() > 180:
                 log.write(str(now) + " Sump Pump has been deemed broaken")
+                print ("Sump Pump is broken")
                 sendEmail("The sump pump is now broken.")
+                print ("An email has been sent saying the sump pump is broken.")
                 break
 
 log.write(str(now) + " The sctipt has stopped.")
+print ("The script has stopped")
 sendEmail("The script has stopped.")
+print ("An email was sent sayig 'The script has stopped'")
 exit
