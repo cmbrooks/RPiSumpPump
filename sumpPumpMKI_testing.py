313b1367cfa0f3250451483972e4344ed495dc69
Cody Brooks <codymb36@gmail.com>
2013-10-22 07:24:03 -0500

    Experimenting with different program structure.

--- sumpPumpMKI_testing.py
+++ sumpPumpMKI_testing.py
@@ -47,29 +47,28 @@ def sendEmail(msg):
     server.quit()
 
 
-while running is True:
-    if floatSwitch is 1:
-        #Write the time and what happened to the file
-        log.write(str(now) + " Float switch turned on")
-        print ("Float switch turned on")
-        #Wait until switch is turned off
+if floatSwitch is 1:
+    #Write the time and what happened to the file
+    log.write(str(now) + " Float switch turned on")
+    print ("Float switch turned on")
+    #Wait until switch is turned off
 
-        while floatSwitch:
-            startTime = time.time()
-            if floatSwitch is 0:
-                log.write(str(now) + " Float switch turned off")
-                print ("Float switch turned off")
-                break
-            #if elapsedTime > 3 min (in the form of 180 seconds)
-            elif elapsedTime() > 180:
-                log.write(str(now) + " Sump Pump has been deemed broaken")
-                print ("Sump Pump is broken")
-                sendEmail("The sump pump is now broken.")
-                print ("An email has been sent saying the sump pump is broken.")
-                break
-            elif raw_input() == "stop":
-                running = False
-                break
+    while floatSwitch:
+        startTime = time.time()
+        if floatSwitch is 0:
+            log.write(str(now) + " Float switch turned off")
+            print ("Float switch turned off")
+            break
+        #if elapsedTime > 3 min (in the form of 180 seconds)
+        elif elapsedTime() > 180:
+            log.write(str(now) + " Sump Pump has been deemed broaken")
+            print ("Sump Pump is broken")
+            sendEmail("The sump pump is now broken.")
+            print ("An email has been sent saying the sump pump is broken.")
+            break
+        elif raw_input() == "stop":
+            running = False
+            break
 
 log.write(str(now) + " The sctipt has stopped.")
 print ("The script has stopped")
